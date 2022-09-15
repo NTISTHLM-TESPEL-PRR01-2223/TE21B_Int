6f648181ffd6f546985920cb11958e51b56b0d51
Mikael Bergström <mikael.bergstrom@ga.ntig.se>
2022-09-15 15:03:18 +0200

    Initial commit

---
+++ TE21B_Int/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+string name = "Micke";
+
+string summa = "45" + "55";
+
+// int potion = 25;
+// int curse = -25;
+
+// int hitpoints = 100 + potion;
+
+// hitpoints += curse;
+
+// hitpoints++;
+
+int xLocation = 5 / 2;
+
+
+
+
+Console.WriteLine(xLocation);
+
+// bool isAlive = name == "Micke";
+
+// if (name == "Micke")
+// {
+//   Console.WriteLine("Yes");
+// }
+
+// Console.WriteLine(isAlive);
+
+Console.ReadLine();
